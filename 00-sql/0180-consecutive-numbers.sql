e61917c24131ad7e23a44cb705312e3ac1c678c5
Esteban Mora <estebanjmora16@gmail.com>
2025-11-28 08:28:30 -0600

    solution(0180-consecutive-numbers): use window LEAD to detect three consecutive equal nums

---
+++ 00-sql/0180-consecutive-numbers.sql
@@ -0,0 +1,12 @@
+-- 0180 Consecutive Numbers
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num_1,
+        LEAD(num, 2) OVER (ORDER BY id) AS next_num_2
+    FROM Logs
+) AS t
+WHERE num = next_num_1
+  AND num = next_num_2;
